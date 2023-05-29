9c5326d05ab6b06ea54b59ac6f00ff1ec5e98423
acidburn7 <varaksinaalla@mail.ru>
2023-05-29 23:11:02 +0300

    init

--- products/tests.py
+++ products/tests.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from products.models import Product, ProductCategory
 
 class IndexViewTestCase(TestCase):
     
@@ -13,3 +14,28 @@ class IndexViewTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Store')
         self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = ['categories.json', 'products.json']
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        products = Product.objects.all()
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Продукты')
+        self.assertTemplateUsed(response, 'products/products.html')
+        self.assertEqual(list(response.context_data['object_lis']), list(products[:3]))
+    
+    def test_list_with_category(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        category = ProductCategory.objects.filter()
+        products = Product.objects.all()
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Продукты')
+        self.assertTemplateUsed(response, 'products/products.html')
+        self.assertEqual(list(response.context_data['object_lis']), list(products[:3]))
